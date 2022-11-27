edcd5c281f237bd951a083f678b2c4ef11b70c47
georgedragoshutanu <george.hutanu-contractor@robertwalters.com>
2022-11-27 22:22:07 +0200

    fix: add login layout mobile

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import LoginPageMobile from '@pages/Login/LoginPageMobile';
+import LoginPageMobile from '@pages/../components/Login/LoginPageMobile';
 import React from 'react';
 
 export default (): JSX.Element => (
