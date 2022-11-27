b6696a7510ccd6a0550f0c20c49890a027f7d4d8
georgedragoshutanu <george.hutanu-contractor@robertwalters.com>
2022-11-27 22:20:19 +0200

    fix: move .env.example file

---
+++ src/components/Login/LoginPageMobile.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default (): JSX.Element => <div className={'login-mobile'}></div>;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+import LoginPageMobile from '@pages/Login/LoginPageMobile';
+import React from 'react';
+
+export default (): JSX.Element => (
+  <>
+    <LoginPageMobile />
+  </>
+);
